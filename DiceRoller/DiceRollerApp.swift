ff9059c6875b57f9ce5da45025109fe10373bc42
Prajakta Kulkarni <your.email@example.com>
2025-09-18 15:46:49 +0200

    Created a custom view

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Prajakta Kulkarni on 18/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
